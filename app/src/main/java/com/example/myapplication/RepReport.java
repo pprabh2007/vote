a06cdf8237392f3bd07aa3e64c306e276779244c
pprabh2007 <pprabh2007@hotmail.co.uk>
2019-04-12 10:29:03 +0530

    styled pie chart

--- app/src/main/java/com/example/myapplication/RepReport.java
+++ app/src/main/java/com/example/myapplication/RepReport.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
@@ -24,6 +25,12 @@ public class RepReport extends AppCompatActivity {
     Bundle extras;
     User THIS_USER_OBJECT;
 
+    TextView per_launched;
+    TextView per_completed;
+    TextView per_assigned;
+    TextView constituency_name;
+    TextView representative_name;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,19 +40,29 @@ public class RepReport extends AppCompatActivity {
         extras=getIntent().getExtras();
         THIS_USER_OBJECT=(User)extras.getSerializable("THIS_USER_OBJECT");
 
-        setupPieChart();
+        constituency_name=findViewById(R.id.constituency_title);
+        representative_name=findViewById(R.id.person_title);
+
+        constituency_name.setText(THIS_USER_OBJECT.getConstituency());
+        representative_name.setText(THIS_USER_OBJECT.getName());
+
+        setupPieChartAndData();
 
     }
 
-    protected void setupPieChart()
+    protected void setupPieChartAndData()
     {
         HashMap<String, Integer> performaceValues=DBHelper.getPerformanceData(THIS_USER_OBJECT.getConstituency());
+
+        int total_complaints=0;
 
         List<PieEntry> pieEntries=new ArrayList<>();
         for(Map.Entry<String, Integer> ordered_pair: performaceValues.entrySet())
         {
             String key=ordered_pair.getKey();
             Integer value=ordered_pair.getValue();
+
+            total_complaints+=value;
 
             pieEntries.add(new PieEntry(value, key));
         }
@@ -57,8 +74,17 @@ public class RepReport extends AppCompatActivity {
 
         PieChart pc=findViewById(R.id.pie_chart);
         pc.setData(pieData);
+        pc.animateY(1000);
         pc.invalidate();
 
+
+        per_assigned=findViewById(R.id.per_assigned);
+        per_completed=findViewById(R.id.per_completed);
+        per_launched=findViewById(R.id.per_launched);
+
+        per_assigned.setText(per_assigned.getText().toString()+ ((performaceValues.get("Assigned")*100.0)/total_complaints));
+        per_completed.setText(per_completed.getText().toString()+ ((performaceValues.get("Completed")*100.0)/total_complaints));
+        per_launched.setText(per_launched.getText().toString()+ ((performaceValues.get("Launched")*100.0)/total_complaints));
     }
 
     protected void onStop()
